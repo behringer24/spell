957453771d75dc31dbfd879965975432cc8f4cab
Andreas Behringer <abe@activecube.de>
2025-04-06 07:47:28 +0200

    ADD: CITE block with inline SVG icon as ::before element

--- parser.go
+++ parser.go
@@ -13,6 +13,7 @@ import (
 const (
 	BLOCKTYPE_NONE int = 0
 	BLOCKTYPE_CODE int = 1
+	BLOCKTYPE_CITE int = 2
 )
 
 var (
@@ -120,21 +121,31 @@ func parseLine(book *epub.EPub, line string, baseDir string, insideBlock bool) s
 		inUlList = false
 		return "</ul>\n" + parseLine(book, line, baseDir, false)
 	} else if blockQuoteRegex.MatchString(line) {
-		log.Printf("blockQuote ``` gefunden")
 		if inBlockType > 0 {
 			log.Printf("blockQuote schließen")
 			inBlockType = 0
 			return "</blockquote>\n"
 		} else {
-			//matches := blockQuoteRegex.FindStringSubmatch(line)
-			//log.Printf("blockQuote opening: %s", matches[2])
+			matches := blockQuoteRegex.FindStringSubmatch(line)
+			blocktype := "code"
 			inBlockType = BLOCKTYPE_CODE
-			return fmt.Sprintf("<blockquote class=\"%s\">\n", "code")
+			if len(matches) == 2 && matches[1] != "" {
+				blocktype = strings.ToLower(matches[1])
+
+				if blocktype == "cite" {
+					inBlockType = BLOCKTYPE_CITE
+				}
+			}
+			log.Printf("blockQuote opening: %s", blocktype)
+			return fmt.Sprintf("<blockquote class=\"%s\">\n", blocktype)
 		}
-	} else if inBlockType != BLOCKTYPE_NONE {
+	} else if inBlockType != BLOCKTYPE_NONE && !insideBlock {
 		if inBlockType == BLOCKTYPE_CODE {
-			log.Printf("blockQuote Codezeile")
+			log.Printf("blockQuote CODE line")
 			return fmt.Sprintf("%s</br>\n", line)
+		} else if inBlockType == BLOCKTYPE_CITE {
+			log.Printf("blockQuote CITE line")
+			return parseLine(book, line, baseDir, true)
 		}
 	} else if chapterRegex.MatchString(line) {
 		// Chapter starting with one # char
--- template.go
+++ template.go
@@ -8,12 +8,15 @@ import (
 
 const defaultCSS = `// Default CSS
 body {
-  font-family: Palatino;
+	font-family: Palatino;
 }
 h1, h2, h3, h4, h5, h6 {
 	font-family: Helvetica;
 	font-variant: small-caps;
+	text-align: left;
 	color: #666666;
+	page-break-after:avoid;
+	page-break-inside: avoid;
 }
 h1 {
 	border: 1px solid #888;
@@ -46,12 +49,29 @@ hr {
 }
 blockquote {
 	margin-left: 3em;
+	margin-right: 3em;
 	padding: 1em;
-	background-color: #aaa;
-	border-radius: 5px;
 }
 blockquote.code {
-	font-family: "monospace"
+	font-family: "monospace";
+	background-color: #ccc;
+	border-radius: 5px;
+	text-align: left;
+}
+blockquote.cite {
+	font-size:120%;
+	border: 1px solid #888;
+	border-width: 1px 0;
+	padding: 0.5em;
+	position: relative;	
+}
+blockquote.cite::before {
+	content: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8' standalone='no'%3F%3E%3Csvg width='14.731999mm' height='10.160001mm' viewBox='0 0 14.731999 10.160001' version='1.1' id='svg1' xmlns='http://www.w3.org/2000/svg' xmlns:svg='http://www.w3.org/2000/svg'%3E%3Cdefs id='defs1' /%3E%3Cg id='layer1' transform='translate(-70.44453,-66.836947)'%3E%3Cpath fill='%23CCC' d='m 72.98453,72.323347 q -0.9652,0 -1.778,-0.6096 -0.762,-0.6604 -0.762,-1.6256 0,-1.4732 1.1176,-2.3368 1.1684,-0.9144 2.6416,-0.9144 1.4732,0 2.286,0.9144 0.8128,0.9144 0.8128,2.3876 0,1.9304 -1.4224,4.2672 -1.5748,2.5908 -3.4036,2.5908 -0.9652,0 -0.9652,-0.8128 0.8636,-0.8128 1.5748,-1.8288 0.762,-1.016 0.9144,-2.2352 z m 7.8232,0 q -0.9144,0 -1.7272,-0.6096 -0.762,-0.6604 -0.762,-1.6256 0,-1.4732 1.1176,-2.3368 1.1684,-0.9144 2.6924,-0.9144 1.4224,0 2.235199,0.9144 0.8128,0.9144 0.8128,2.3876 0,2.032 -1.371599,4.2672 -1.5748,2.5908 -3.4544,2.5908 -0.9652,0 -0.9652,-0.8128 0.8128,-0.8128 1.524,-1.778 0.762,-0.9652 0.9652,-2.286 -0.6096,0.2032 -1.0668,0.2032 z' /%3E%3C/g%3E%3C/svg%3E%0A");
+	position: absolute;
+	height: 80px;
+	top: -25px;
+	left: -25px;
+  	z-index: -1;
 }
 span.code {
 	background-color: #aaa;
