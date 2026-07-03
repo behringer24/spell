b3477c97c847e86c4dd2cfed86666a33ee0e80a7
Andreas Behringer <abe@activecube.de>
2026-07-03 23:23:50 +0200

    Increased version number to v1.5.1

--- spell.go
+++ spell.go
@@ -14,7 +14,7 @@ import (
 const (
 	title       = "spell"
 	description = "Smart Processing and Enhanced Lightweight Layout. Command line parser for converting enhanced markdown to epub."
-	version     = "v1.5.0"
+	version     = "v1.5.1"
 )
 
 var (
